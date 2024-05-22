30de33c661f90df44f3494b5f07878600f458a4b
Olga Tokarenko <olga@tokarenko.net>
2024-05-22 13:45:23 +0300

    Add prettier, format files

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function multiply(a, b) {
     return a * b;
 }
 
-console.log("Sum: ", sum(2, 3));
-console.log("Subtract: ", subtract(5, 2));
-console.log("Multiply: ", multiply(4, 2));
+console.log('Sum: ', sum(2, 3));
+console.log('Subtract: ', subtract(5, 2));
+console.log('Multiply: ', multiply(4, 2));
