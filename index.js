7c4e448681b02fe4de2a4402b0f281beed8a01de
Olga Tokarenko <olga@tokarenko.net>
2024-05-22 13:49:37 +0300

    Add ESLint with Airbnb configuration and fix linting issues

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
+// eslint-disable-next-line no-console
 console.log('Sum: ', sum(2, 3));
+// eslint-disable-next-line no-console
 console.log('Subtract: ', subtract(5, 2));
+// eslint-disable-next-line no-console
 console.log('Multiply: ', multiply(4, 2));
