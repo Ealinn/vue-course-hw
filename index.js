3b677ea735fdf4781526351f0e4dffe043dfd2f6
Olga Tokarenko <olga@tokarenko.net>
2024-05-22 13:31:57 +0300

    Add index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log("Sum: ", sum(2, 3));
+console.log("Subtract: ", subtract(5, 2));
+console.log("Multiply: ", multiply(4, 2));
