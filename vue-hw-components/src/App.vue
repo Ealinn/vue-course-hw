3444be37e78f6bac8c73e71814a53c6293ecb63f
Olga Tokarenko <olga@tokarenko.net>
2024-05-25 01:34:27 +0300

    Create Card component

---
+++ vue-hw-components/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+  <CardComponent />
+</template>
+
+<script>
+import CardComponent from './components/CardComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CardComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.logo {
+  width: 100px;
+  height: 100px;
+}
+</style>
---
+++ vue-hw-components/src/components/CardComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div className="card">
+    <div className="card-body">
+      <h4 className="card-title">Card title</h4>
+      <p className="card-text">Some quick example text to build on the card</p>
+      <button type="button" className="btn btn-primary">Go somewhere</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardComponent',
+};
+</script>
+
+<style scoped>
+.card {
+  width: 100%;
+  max-width: 500px;
+  border-radius: 4px;
+  margin: 50px auto;
+  box-shadow: 0px 0px 15px rgba(32, 49, 95, 0.1);
+}
+.card-body {
+  padding: 16px;
+}
+.card-title {
+  font-size: 20px;
+  margin-top: 0;
+  margin-bottom: 16px;
+}
+.card-text {
+  margin-top: 0;
+  margin-bottom: 16px;
+}
+.btn {
+  background-color: #156BB7;
+  color: white;
+  border: none;
+  padding: 7px 16px;
+  text-align: center;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 4px;
+}
+.btn:hover {
+  background-color: #0B5596;
+}
+</style>
