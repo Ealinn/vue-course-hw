3e039cced2ff44c85c7cb93c492673881dca8d59
Olga Tokarenko <olga@tokarenko.net>
2024-05-25 02:04:34 +0300

    Create Card component with props

---
+++ vue-hw-components-props/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+
+  <!-- First render only with title -->
+  <CardComponent title="Hi" />
+
+  <!-- Second render only with text -->
+  <CardComponent text="How are you?" />
+
+  <!-- Third render with title and text -->
+  <CardComponent title="Hi" text="How are you?" />
+</template>
+
+<script>
+import CardComponent from './components/CardComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CardComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.logo {
+  width: 100px;
+  height: 100px;
+}
+</style>
---
+++ vue-hw-components-props/src/components/CardComponent.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card" v-if="title || text">
+    <div class="card-body">
+      <h4 class="card-title" v-if="title">{{ title }}</h4>
+      <p class="card-text" v-if="text">{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardComponent',
+  props: {
+    title: {
+      type: String,
+      default: null,
+    },
+    text: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  width: 100%;
+  max-width: 500px;
+  border-radius: 4px;
+  margin: 50px auto;
+  box-shadow: 0px 0px 15px rgba(32, 49, 95, 0.1);
+}
+.card-body {
+  padding: 16px;
+}
+.card-title {
+  font-size: 20px;
+  margin-top: 0;
+  margin-bottom: 16px;
+}
+.card-text {
+  margin-top: 0;
+  margin-bottom: 16px;
+}
+.btn {
+  background-color: #156bb7;
+  color: white;
+  border: none;
+  padding: 7px 16px;
+  text-align: center;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 4px;
+}
+.btn:hover {
+  background-color: #0b5596;
+}
+</style>
