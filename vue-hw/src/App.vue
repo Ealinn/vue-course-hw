80feb639bcf24098f40b672d42ce31d605e05387
Olga Tokarenko <olga@tokarenko.net>
2024-06-04 23:40:25 +0300

    Create Definitions component

--- vue-hw/src/App.vue
+++ vue-hw/src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+    <DefinitionsComponent :definitions="definitions" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DefinitionsComponent from "./components/DefinitionsComponent.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        DefinitionsComponent,
+    },
+    data() {
+        return {
+            definitions: [
+                { dt: "one", dd: "two", id: 1 },
+                { dt: "another term", dd: "another description", id: 2 },
+            ],
+        };
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+.logo {
+    width: 100px;
+    height: 100px;
 }
 </style>
---
+++ vue-hw/src/components/DefinitionsComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <dl class="definitions-list">
+        <template v-for="definition in definitions" :key="definition.id">
+            <dt>{{ definition.dt }}</dt>
+            <dd>{{ definition.dd }}</dd>
+        </template>
+    </dl>
+</template>
+
+<script>
+export default {
+    name: "DefinitionsComponent",
+    props: {
+        definitions: {
+            type: Array,
+            default() {
+                return [];
+            },
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped>
+.definitions-list {
+    margin-top: 50px;
+}
+</style>
