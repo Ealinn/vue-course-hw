afaca3bf84b9671d84bb7602d05872da9d0795c1
Olga Tokarenko <olga@tokarenko.net>
2024-06-05 01:31:42 +0300

    Create ListGroup component

---
+++ vue-hw/src/components/ListGroup.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="d-flex justify-content-center">
+        <ul class="list-group">
+            <li
+                class="list-group-item"
+                v-for="(child, index) in items"
+                :key="index"
+            >
+                <slot :child="child"></slot>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ListGroup",
+    props: {
+        items: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped>
+.list-group {
+    width: 50%;
+    margin-top: 60px;
+}
+</style>
