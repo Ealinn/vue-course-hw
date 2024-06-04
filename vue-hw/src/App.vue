ef43a5af8208dd28ddce1743d81692b76d7eed69
Olga Tokarenko <olga@tokarenko.net>
2024-06-05 00:13:58 +0300

    Create ProgressBar component

--- vue-hw/src/App.vue
+++ vue-hw/src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+        <ProgressBar :percentage="40" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProgressBar from "./components/ProgressBar.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        ProgressBar,
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+.logo {
+    width: 100px;
+    height: 100px;
 }
 </style>
---
+++ vue-hw/src/components/ProgressBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="d-flex justify-content-center">
+        <div class="progress mt-5 w-75">
+            <div
+                class="progress-bar"
+                role="progressbar"
+                :aria-valuenow="percentage"
+                aria-valuemin="0"
+                aria-valuemax="100"
+                aria-label="progressbar"
+                :style="{ width: percentage + '%' }"
+            ></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ProgressBar",
+    props: {
+        percentage: {
+            type: Number,
+            required: true,
+            validator(value) {
+                return value >= 0 && value <= 100;
+            },
+        },
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
