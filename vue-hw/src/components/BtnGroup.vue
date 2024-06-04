a5b3aa75089a202c9696eb12c8a8b0ece6e3f525
Olga Tokarenko <olga@tokarenko.net>
2024-06-05 01:46:03 +0300

    Create BtnGroup component

---
+++ vue-hw/src/components/BtnGroup.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="d-flex justify-content-center mt-5">
+        <div class="btn-group" role="group">
+            <button
+                type="button"
+                :class="[
+                    'btn',
+                    'btn-secondary',
+                    'left',
+                    { active: isActive === 'left' },
+                ]"
+                @click="setActiveButton('left')"
+            >
+                Left
+            </button>
+            <button
+                type="button"
+                :class="[
+                    'btn',
+                    'btn-secondary',
+                    'right',
+                    { active: isActive === 'right' },
+                ]"
+                @click="setActiveButton('right')"
+            >
+                Right
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BtnGroup",
+    data() {
+        return {
+            isActive: null,
+        };
+    },
+    methods: {
+        setActiveButton(button) {
+            this.isActive = button;
+        },
+    },
+};
+</script>
+
+<style scoped></style>
