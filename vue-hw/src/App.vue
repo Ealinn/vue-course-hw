17c13d28ed8f809f5f9f48ea46a590c04a67ebd1
Olga Tokarenko <olga@tokarenko.net>
2024-06-05 03:00:36 +0300

    Create Carousel component

--- vue-hw/src/App.vue
+++ vue-hw/src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+    <CarouselComponent :images="images" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CarouselComponent from "./components/CarouselComponent.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        CarouselComponent,
+    },
+    data() {
+        return {
+            images: [
+                "/images/cat1.jpg",
+                "/images/cat2.jpg",
+                "/images/cat3.jpg",
+                "/images/cat4.jpg",
+            ],
+        };
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+.logo {
+    width: 100px;
+    height: 100px;
 }
 </style>
---
+++ vue-hw/src/components/CarouselComponent.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="d-flex justify-content-center mt-5">
+        <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div
+                    v-for="(image, index) in images"
+                    :key="index"
+                    :class="[
+                        'carousel-item',
+                        { active: index === currentIndex },
+                    ]"
+                >
+                    <img :src="image" class="d-block w-100" alt="" />
+                </div>
+            </div>
+            <button
+                class="carousel-control-prev"
+                @click="prevImage"
+                type="button"
+            >
+                <span
+                    class="carousel-control-prev-icon"
+                    aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+                class="carousel-control-next"
+                @click="nextImage"
+                type="button"
+            >
+                <span
+                    class="carousel-control-next-icon"
+                    aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CarouselComponent",
+    props: {
+        images: {
+            type: Array,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            currentIndex: 0,
+        };
+    },
+    methods: {
+        nextImage() {
+            this.currentIndex = (this.currentIndex + 1) % this.images.length;
+        },
+        prevImage() {
+            this.currentIndex =
+                (this.currentIndex - 1 + this.images.length) %
+                this.images.length;
+        },
+    },
+};
+</script>
+
+<style scoped>
+.carousel {
+    width: 60%;
+    border-radius: 8px;
+    overflow: hidden;
+}
+</style>
