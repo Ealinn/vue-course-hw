10414fa997ce6d8895efd735034805ed768ddf46
Olga Tokarenko <olga@tokarenko.net>
2024-06-15 20:18:18 +0300

    Create Collapse component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container min-vh-100">
+        <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+        <h1 class="title">Приклад компонента Collapse</h1>
+        <CollapseComponent text="Це прихований текст" :opened="true" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CollapseComponent from "./components/CollapseComponent.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        CollapseComponent,
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    text-align: center;
+}
+.logo {
+    width: 100px;
+    height: 100px;
+}
+.title {
+    margin-top: 50px;
 }
 </style>
---
+++ src/components/CollapseComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <div v-if="text" class="collapse-component">
+        <p>
+            <a
+                class="btn btn-primary"
+                href="#"
+                role="button"
+                @click.prevent="toggleCollapse"
+            >
+                {{ isOpen ? "Hide" : "Show" }}
+            </a>
+        </p>
+        <div :class="['collapse', { show: isOpen }]">
+            <div class="card card-body">
+                {{ text }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CollapseComponent",
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+        opened: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    data() {
+        return {
+            isOpen: this.opened,
+        };
+    },
+    methods: {
+        toggleCollapse() {
+            this.isOpen = !this.isOpen;
+        },
+    },
+};
+</script>
+
+<style>
+.collapse-component {
+    margin-top: 50px;
+}
+</style>
