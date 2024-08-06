05beba1fb2acdc90f1b2f91564e4ada6bc355cbf
Olga Tokarenko <olga@tokarenko.net>
2024-08-07 01:24:05 +0300

    Fixed bug with index in TodoList pinia store

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 import App from './App.vue'
+import router from "./router.js";
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+app.use(router)
+app.use(pinia)
+app.mount('#app')
